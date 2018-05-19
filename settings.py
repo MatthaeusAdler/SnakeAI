32df812a5856d8132a781ee7bc9f3ec184ea945a
Tobias Sittenauer <tobias@sittenauer.eu>
2018-05-19 15:09:36 +0200

    removing useless comments in settings.py

--- settings.py
+++ settings.py
@@ -3,8 +3,3 @@ GAME_SIZE = 10
 TILE_SIZE = 64
 PLAYGROUND = (TILE_SIZE * GAME_SIZE, TILE_SIZE * GAME_SIZE)
 FPS = 7
-
-# font_family = "Bauhaus 93"
-# font_size_big = 45
-# font_size_medium = int(font_size_big / 2)
-# font_size_small = int(font_size_big / 4)
